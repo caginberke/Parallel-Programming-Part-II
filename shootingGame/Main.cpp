66085b5baaf5a7b7c5394bdeea3bf48806d403bc
caginberke <caginberke416@gmail.com>
2024-11-08 00:05:10 +0300

    Proje dosyası ekle.

---
+++ shootingGame/Main.cpp
@@ -0,0 +1,50 @@
+#include"icb_gui.h"
+void ICGUI_Create()
+{
+	ICG_MWTitle("Shooting Game");
+	ICG_MWSize(700, 700);
+}
+
+ICBYTES A;
+int keypressed = 0;
+int FRM1, BTN;
+void* gameStart(PVOID lpParam)
+{
+	// CreateMatrix(A, X, Y, Z, W, ICB_UINT);
+	ICBYTES map;
+	CreateImage(map, 560, 620, ICB_UINT);
+	int x = 230, y = 590;
+	while (TRUE)
+	{
+		FillRect(map, x, y, 20, 6, 0xffff00);
+
+		DisplayImage(FRM1, map);
+		Sleep(10);
+		FillRect(map, x, y , 30, 30, 0);
+		if (keypressed == 39)
+			x++;
+		else if (keypressed == 37)
+			x--;
+	}
+	return(NULL);
+}
+void WhenKeyPressed(int k)
+{
+	keypressed = k;
+}
+
+void Start()
+{
+	DWORD dw;
+
+	ICG_SetWindowText(BTN, "TIKLAMA");
+	CreateThread(NULL, 0, (LPTHREAD_START_ROUTINE)gameStart, NULL, 0, &dw);
+
+	SetFocus(ICG_GetMainWindow());
+}
+void ICGUI_main()
+{
+	BTN = ICG_Button(570, 5, 100, 25, "BAÞLAT", Start);
+	FRM1 = ICG_FrameMedium(5, 5, 560, 620);
+	ICG_SetOnKeyPressed(WhenKeyPressed);
+}
